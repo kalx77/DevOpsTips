1db97bba6abbbe6dfd776502c71523f570069324
Aleksei Kuzmin <kuzmin@mindbox.ru>
2021-09-28 17:08:34 +0300

    upgrade add_partitions script

--- python_scripts/add_partitions_to_topics.py
+++ python_scripts/add_partitions_to_topics.py
@@ -1,4 +1,5 @@
 # Need kafka-python package
+from time import sleep
 from kafka import KafkaAdminClient
 from kafka import KafkaConsumer
 from kafka.admin import NewPartitions
@@ -7,6 +8,8 @@ import re
 bootstrap_servers = "kafka-customers-c-1-v1.corp.itcd.ru"
 topic_pattern = ".*CDP.Scenarios.Runtime"
 need_partitions = 4
+sleep_after = 20
+sleeping_time = 20
 
 admin_client = KafkaAdminClient(
   bootstrap_servers = bootstrap_servers
@@ -19,14 +22,23 @@ consumer = KafkaConsumer(
 
 topics = consumer.topics()
 cdp_topics = [topic for topic in consumer.topics() if re.match(topic_pattern, topic)]
+topics_amount = len(cdp_topics)
+counter = 0
 
 for topic in cdp_topics:
   partitions = len(consumer.partitions_for_topic(topic))
   if partitions < need_partitions:
     print(f'Increase number of partitions in topic {topic} to {need_partitions}')
-    # topic_partitions = {}
-    # topic_partitions[topic] = NewPartitions(total_count=need_partitions)
-    # admin_client.create_partitions(topic_partitions)
+    counter += 1
+    topic_partitions = {}
+    topic_partitions[topic] = NewPartitions(total_count=need_partitions)
+    admin_client.create_partitions(topic_partitions)
+
+    if counter % sleep_after == 0 and counter != 0 and counter <= topics_amount:
+      print(f"Sleeping {sleeping_time} seconds")
+      sleep(sleeping_time)
+
+print(f"All of {counter} from {topics_amount} topics upgraded!")
 
 consumer.close()
 admin_client.close()
